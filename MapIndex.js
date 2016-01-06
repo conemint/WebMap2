1dec922088cad017e225d602cb867dc0b3b0920f
conemint <min.mindyzhu@gmail.com>
2016-01-06 16:51:50 -0500

    Create MapIndex.js
    as of 01.05

---
+++ MapIndex.js
@@ -0,0 +1,211 @@
+var map, url;
+
+      require([
+	        "esri/map",
+      "esri/layers/FeatureLayer",
+      
+      "esri/dijit/Search",
+      "esri/InfoTemplate",
+      
+      "esri/symbols/SimpleLineSymbol",
+      "esri/symbols/SimpleFillSymbol",
+      "esri/symbols/TextSymbol",
+      "esri/renderers/SimpleRenderer",
+
+      "esri/layers/LabelLayer",
+
+      "esri/Color",
+      
+      "esri/graphic", 
+      "esri/lang",
+      
+      "dojo/number", 
+      "dojo/dom-style", 
+      "dijit/TooltipDialog", 
+      "dijit/popup", 
+	  "dijit/layout/ContentPane", 
+	  "dojo/number",
+	  "dijit/form/ToggleButton",
+	  "dojo/fx/Toggler", "dojo/fx", "dojo/dom", "dojo/on",
+	  
+	  "esri/geometry/Extent",
+	  "esri/SpatialReference",
+	  
+      "dojo/cookie", "dojo/json", "dojo/domReady!"
+		], function(Map, FeatureLayer, Search, InfoTemplate,
+        SimpleLineSymbol, SimpleFillSymbol, TextSymbol, SimpleRenderer,
+        LabelLayer, Color, Graphic, esriLang, number, domStyle, 
+        TooltipDialog, dijitPopup, ContentPane, localeNumber,ToggleButton,
+		Toggler, coreFx, dom, on, Extent, SpatialReference, cookie, json) {
+        map = new Map("map", {
+          basemap: "gray",  //For full list of pre-defined basemaps, navigate to //arcg.is/1JVo6Wd
+          center: [-74.005941, 40.712784] //, // longitude, latitude
+        });
+		
+		
+       
+        map.on("load", function() {
+            map.disablePan();
+            map.disableMapNavigation();
+            map.hidePanArrows();
+            map.hideZoomSlider();
+			map.graphics.enableMouseEvents();
+			map.graphics.on("mouse-out", function(){ 
+				map.graphics.clear();
+				map.setMapCursor("default");
+				url = '';
+				boroName = '';
+			});
+			//graphics on click, open page
+			map.graphics.on("click", graphicsClickHandler);
+          });
+		  
+		var myExtent = new esri.geometry.Extent(-74.305941, 40.495004, -73.705941, 40.965382,
+			new esri.SpatialReference({wkid:4326}) );		  
+		map.setExtent(myExtent);
+          
+		var layer = new FeatureLayer("//services3.arcgis.com/aD88pT4hjL80xq0F/arcgis/rest/services/simplenybb/FeatureServer/0", {
+            outFields: ["BoroName", "Link" ],
+            minScale:0,
+            maxScale:0
+         });
+		  
+		map.addLayer(layer);
+		
+		
+		var layer_ann = new FeatureLayer("//services3.arcgis.com/aD88pT4hjL80xq0F/arcgis/rest/services/nybb_ano/FeatureServer/0", {
+            outFields: ["*"],
+            minScale:0,
+            maxScale:0
+         });
+		 var symbol = new SimpleFillSymbol().setColor(new Color([25,25,25,1]));
+		 symbol.setOutline(new SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new Color([0,0,0,0]), 2));
+		 var rendererN = new SimpleRenderer(symbol);
+ 		 layer_ann.setRenderer(rendererN);  
+		map.addLayer(layer_ann);
+		
+		///HOVER FUNCTION
+		var highlightSymbol = new SimpleFillSymbol(
+          SimpleFillSymbol.STYLE_SOLID, 
+          new SimpleLineSymbol(
+            SimpleLineSymbol.STYLE_SOLID, 
+            new Color([50,50,50]), 1
+          ), 
+          new Color([125,125,125,0.35])
+        );
+        //listen for when the onMouseOver event fires on the layer
+        layer.on("mouse-over", function(evt){
+			map.graphics.clear();
+			map.setMapCursor("pointer"); //Add mouse change at hover
+			var highlightGraphic = new Graphic(evt.graphic.geometry,highlightSymbol);
+			url = evt.graphic.attributes.Link
+			boroName = evt.graphic.attributes.BoroName
+            map.graphics.add(highlightGraphic);
+        });
+		
+		//Add Cookie for boro clicked
+		function writeBoroCookie(curBoro){
+			c_boro=curBoro;
+			if (!curBoro){
+				c_boro="";
+			}
+			if (navigator.cookieEnabled){
+				mapBoro = {boro: c_boro};
+				cookie("mapBoro",json.stringify(mapBoro));
+				curTime = Date.now();
+				cookie("mapPID", json.stringify(curTime));
+			}
+			return mapBoro;
+		}
+
+		//open url, for map on click
+		function graphicsClickHandler(e){
+			if (url == ''){
+				return false;
+			};
+			var newURL="";
+			if (pmapOn){
+				if (boroName == "Manhattan"){
+					newURL = "http://www1.nyc.gov/assets/finance/jump/property-maps/mh_fy16final.html";
+				}
+				else if (boroName == "Bronx"){
+					newURL = "http://www1.nyc.gov/assets/finance/jump/property-maps/bx_fy16final.html";
+				}
+				else if (boroName == "Brooklyn"){
+					newURL = "http://www1.nyc.gov/assets/finance/jump/property-maps/bk_fy16final.html";
+				}
+				else if (boroName == "Queens"){
+					newURL = "http://www1.nyc.gov/assets/finance/jump/property-maps/qn_fy16final.html";
+				}
+				else if (boroName == "Staten Island"){
+					newURL = "http://www1.nyc.gov/assets/finance/jump/property-maps/si_fy16final.html";
+				}
+				else {
+					newURL = url;
+				}
+			}
+			else {
+				if (url.indexOf("mh")>=0){
+					curBoro = "1";
+				}
+				else if (url.indexOf("bx")>=0){
+					curBoro = "2";
+				}
+				else if (url.indexOf("bk")>=0){
+					curBoro = "3";
+				}
+				else if (url.indexOf("qn")>=0){
+					curBoro = "4";
+				}
+				else if (url.indexOf("si")>=0){
+					curBoro = "5";
+				}
+				else {
+					curBoro ="1";
+				}
+				writeBoroCookie(curBoro);
+				newURL = "compmap"; //Change for the uploaded url for comptool;
+			}
+			window.open(newURL,'_blank');			
+		}
+		var pmapOn = true;
+		var rpts = false;
+		on(dojo.byId("divtagp1"),"click",function(e){
+			dom.byId('ReportPage').style.display = "none";
+			dom.byId('map').style.display = "block";
+			if(!pmapOn){
+				domStyle.set("divtagp1",{"background-color": "#0052A3","color":"white"});
+				domStyle.set("divtagp3",{"background-color": "white","color":"#0052A3"});
+				domStyle.set("divtagp2",{"background-color": "white","color":"#0052A3"});
+				pmapOn = true;
+				rpts = false;
+				dom.byId('divnyctitle').innerHTML = "| Search Map for Tax Class 1 Single to Three Family Homes";
+				dom.byId('instruction').innerHTML = "Please click on a borough to continue searching:";
+			}
+		});
+		on(dojo.byId("divtagp2"),"click",function(e){
+			if(!rpts){
+				dom.byId('map').style.display = "none";
+				dom.byId('ReportPage').style.display = "block";				
+				domStyle.set("divtagp2",{"background-color": "#0052A3","color":"white"});
+				domStyle.set("divtagp1",{"background-color": "white","color":"#0052A3"});
+				domStyle.set("divtagp3",{"background-color": "white","color":"#0052A3"});
+				rpts=true;
+				pmapOn = false;
+				}
+		});
+		on(dojo.byId("divtagp3"),"click",function(e){
+		    var workable = isWorkable();
+			dom.byId('ReportPage').style.display = "none";
+			dom.byId('map').style.display = "block";
+			if((pmapOn || rpts)&& workable){
+				domStyle.set("divtagp3",{"background-color": "#0052A3","color":"white"});
+				domStyle.set("divtagp1",{"background-color": "white","color":"#0052A3"});
+				domStyle.set("divtagp2",{"background-color": "white","color":"#0052A3"});
+				pmapOn = false;
+				rpts = false;
+				dom.byId('divnyctitle').innerHTML = "| Comparable Sales Tool for Tax Class 1 Single to Three Family Homes";
+				dom.byId('instruction').innerHTML = "Please click on a borough to find comparable sales:";
+			}
+		});
+      });
